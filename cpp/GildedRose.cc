29b7a033b2da5f792b4a8215748d87b981e56dfe
mnurzyns <marcin.nurzynski@nokia.com>
2019-10-25 12:34:46 +0200

    changed postincrementation to preincrementation

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -17,7 +17,7 @@ void GildedRose::updateQuality()
             {
                 if (items[i].name != "Sulfuras, Hand of Ragnaros")
                 {
-                    items[i].quality = items[i].quality - 1;
+                    --items[i].quality;
                 }
             }
         }
@@ -25,18 +25,18 @@ void GildedRose::updateQuality()
         {
             if (items[i].quality < 50)
             {
-                items[i].quality = items[i].quality + 1;
+                ++items[i].quality;
 
                 if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (items[i].sellIn < 11)
                     {
-                        items[i].quality = items[i].quality + 1;
+                        ++items[i].quality;
                     }
 
                     if (items[i].sellIn < 6)
                     {
-                        items[i].quality = items[i].quality + 1;
+                        ++items[i].quality;
                     }
                 }
             }
@@ -44,7 +44,7 @@ void GildedRose::updateQuality()
 
         if (items[i].name != "Sulfuras, Hand of Ragnaros")
         {
-            items[i].sellIn = items[i].sellIn - 1;
+            --items[i].sellIn;
         }
 
         if (items[i].sellIn < 0)
@@ -57,20 +57,20 @@ void GildedRose::updateQuality()
                     {
                         if (items[i].name != "Sulfuras, Hand of Ragnaros")
                         {
-                            items[i].quality = items[i].quality - 1;
+                            --items[i].quality;
                         }
                     }
                 }
                 else
                 {
-                    items[i].quality = items[i].quality - items[i].quality;
+                    items[i].quality = 0;
                 }
             }
             else
             {
                 if (items[i].quality < 50)
                 {
-                    items[i].quality = items[i].quality + 1;
+                    ++items[i].quality;
                 }
             }
         }
